2b7c48728f5c440705ca2aee5984ebd454b11e5e
Markum Reed <markumreed@gmail.com>
2024-08-29 11:39:51 -0400

    Task 1 - Customer Information

---
+++ cc_example.js
@@ -0,0 +1,12 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Bob Marley";
+const customerID = 618;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
+
+
+
